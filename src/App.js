08cae48c6bb16f79973e510e5e40e6da24d295c0
abby <abbychoi97@gmail.com>
2022-12-16 14:13:26 -0800

    completed toggling of style through isComplete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import { useState } from 'react';
 
 const TASKS = [
   {
@@ -16,13 +17,47 @@ const TASKS = [
 ];
 
 const App = () => {
+  const initialCopy = TASKS.map((task) => {
+    return { ...task };
+  });
+
+  console.log(initialCopy);
+
+  const [tasksList, setTasksList] = useState(initialCopy);
+
+  // const updateComplete = (updatedTask) => {
+  //   console.log('update task called');
+  //   const newTasksList = tasksList.map((task) => {
+  //     if (task.id === updatedTask.id) {
+  //       return updatedTask;
+  //     } else {
+  //       return task;
+  //     }
+  //   });
+  //   setTasksList(newTasksList);
+  // };
+
+  const updateComplete = (taskId) => {
+    console.log('update task called');
+    const newTasksList = tasksList.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isComplete: !task.isComplete };
+      } else {
+        return task;
+      }
+    });
+    setTasksList(newTasksList);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>
+          <TaskList tasks={tasksList} updateComplete={updateComplete} />
+        </div>
       </main>
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,19 +1,49 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+//destructuring to access props
+// const Task = ({ id, title, isComplete }) => {
+//   const [complete, setComplete] = useState(isComplete);
+//   const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+
+//   return (
+//     <li className="tasks__item" key={id}>
+//       <button
+//         className={`tasks__item__toggle ${buttonClass}`}
+//         onClick={() => setComplete(!complete)}
+//       >
+//         {title}
+//       </button>
+//       <button className="tasks__item__remove button">x</button>
+//     </li>
+//   );
+// };
+
+// directly accesses through props
+const Task = (props) => {
+  const updateComplete = props.updateComplete;
+  const onCompleteToggle = () => {
+    // const updatedTask = {
+    //   id: props.id,
+    //   title: props.title,
+    //   isComplete: !props.isComplete,
+    // };
+    // updateComplete(updatedTask);
+    updateComplete(props.id);
+    console.log(props.isComplete);
+  };
+
+  const buttonClass = props.isComplete ? 'tasks__item__toggle--completed' : '';
 
   return (
-    <li className="tasks__item">
+    <li className="tasks__item" key={props.id}>
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        onClick={() => onCompleteToggle()}
       >
-        {title}
+        {props.title}
       </button>
       <button className="tasks__item__remove button">x</button>
     </li>
@@ -24,6 +54,7 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  updateComplete: PropTypes.func.isRequired,
 };
 
 export default Task;
