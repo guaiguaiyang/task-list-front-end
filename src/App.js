4d9d3cffe6eee1826d112344afc78c9395d45098
abby <abbychoi97@gmail.com>
2022-12-19 11:33:21 -0800

    completed wave 2 and wave 3

--- src/App.js
+++ src/App.js
@@ -1,52 +1,87 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+// const TASKS = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: true,
+//   },
+// ];
 
 const App = () => {
-  const initialCopy = TASKS.map((task) => {
-    return { ...task };
-  });
+  const URL = 'https://task-list-api-c17.herokuapp.com/tasks';
 
-  console.log(initialCopy);
+  const [tasksList, setTasksList] = useState([]);
 
-  const [tasksList, setTasksList] = useState(initialCopy);
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((response) => {
+        console.log(response);
+        const taskAPIResCopy = response.data.map((task) => {
+          return {
+            id: task.id,
+            title: task.title,
+            isComplete: task.is_complete,
+          };
+        });
+        console.log(taskAPIResCopy);
+        setTasksList(taskAPIResCopy);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-  // const updateComplete = (updatedTask) => {
-  //   console.log('update task called');
-  //   const newTasksList = tasksList.map((task) => {
-  //     if (task.id === updatedTask.id) {
-  //       return updatedTask;
-  //     } else {
-  //       return task;
-  //     }
-  //   });
-  //   setTasksList(newTasksList);
-  // };
-
-  const updateComplete = (taskId) => {
+  const updateComplete = (taskId, isComplete) => {
     console.log('update task called');
-    const newTasksList = tasksList.map((task) => {
-      if (task.id === taskId) {
-        return { ...task, isComplete: !task.isComplete };
-      } else {
-        return task;
-      }
-    });
-    setTasksList(newTasksList);
+
+    const completionURL = isComplete
+      ? `${URL}/${taskId}/mark_incomplete`
+      : `${URL}/${taskId}/mark_complete`;
+
+    axios
+      .patch(completionURL)
+      .then(() => {
+        const newTasksList = tasksList.map((task) => {
+          if (task.id === taskId) {
+            return { ...task, isComplete: !task.isComplete };
+          } else {
+            return task;
+          }
+        });
+        setTasksList(newTasksList);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const deleteTask = (taskId) => {
+    console.log(`inside deleteTask, ${taskId}`);
+    const newTasksList = [];
+    axios
+      .delete(`${URL}/${taskId}`)
+      .then(() => {
+        for (const task of tasksList) {
+          if (task.id !== taskId) {
+            newTasksList.push(task);
+          }
+        }
+        setTasksList(newTasksList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
@@ -56,7 +91,11 @@ const App = () => {
       </header>
       <main>
         <div>
-          <TaskList tasks={tasksList} updateComplete={updateComplete} />
+          <TaskList
+            tasks={tasksList}
+            updateComplete={updateComplete}
+            deleteTask={deleteTask}
+          />
         </div>
       </main>
     </div>
--- src/components/Task.js
+++ src/components/Task.js
@@ -3,35 +3,13 @@ import PropTypes from 'prop-types';
 
 import './Task.css';
 
-//destructuring to access props
-// const Task = ({ id, title, isComplete }) => {
-//   const [complete, setComplete] = useState(isComplete);
-//   const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
-
-//   return (
-//     <li className="tasks__item" key={id}>
-//       <button
-//         className={`tasks__item__toggle ${buttonClass}`}
-//         onClick={() => setComplete(!complete)}
-//       >
-//         {title}
-//       </button>
-//       <button className="tasks__item__remove button">x</button>
-//     </li>
-//   );
-// };
-
 // directly accesses through props
 const Task = (props) => {
   const updateComplete = props.updateComplete;
+  const deleteTask = props.deleteTask;
+
   const onCompleteToggle = () => {
-    // const updatedTask = {
-    //   id: props.id,
-    //   title: props.title,
-    //   isComplete: !props.isComplete,
-    // };
-    // updateComplete(updatedTask);
-    updateComplete(props.id);
+    updateComplete(props.id, props.isComplete);
     console.log(props.isComplete);
   };
 
@@ -45,7 +23,12 @@ const Task = (props) => {
       >
         {props.title}
       </button>
-      <button className="tasks__item__remove button">x</button>
+      <button
+        className="tasks__item__remove button"
+        onClick={() => deleteTask(props.id)}
+      >
+        x
+      </button>
     </li>
   );
 };
@@ -55,6 +38,7 @@ Task.propTypes = {
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
   updateComplete: PropTypes.func.isRequired,
+  deleteTask: PropTypes.func.isRequired,
 };
 
 export default Task;
